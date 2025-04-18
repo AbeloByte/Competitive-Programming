257c7c1abb3f14071d56a8b23b7a456ddf436595
Abel Adane <129602637+AbeloByte@users.noreply.github.com>
2025-04-18 22:39:00 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (55.76%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        arr = []
+        for val in nums :
+            arr.append(nums[val])
+        return arr
+
+        
+
+        
